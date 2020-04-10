21f1aa5d81f640605b97dbda653f9d2119650c1f
Xavier Ventura <xavierventura70@gmail.com>
2020-04-10 20:55:26 +0200

    versio en selenium

---
+++ practica1selenium.py
@@ -0,0 +1,115 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+import pandas as pd
+import re
+
+
+# La implentació de l'Scrapping amb Selenium te aquests requeriments:
+
+# WebDriver (en aquest cas hem triat Chrome)
+#
+# https://chromedriver.chromium.org/getting-started
+#
+
+
+# Crear una nova instancia de Chrome en mode incognit
+print("Obrint el navegador...." , end = " ")
+
+option = webdriver.ChromeOptions()
+option.add_argument("--incognito")
+
+navegador = webdriver.Chrome(executable_path='./chromedriver', options=option)
+
+print ("fet.")
+
+def obtenir_urls_paisos_UN():
+
+  return ["http://data.un.org/en/iso/af.html"]
+
+  # Obtenir la llista de països a partir de la pàgina index
+  navegador.get("http://data.un.org/en/index.html")
+
+  # Els paisos es mostren en una llista amb la següent estructura
+  #<section>
+  #  <ul id='myUL'>
+  #    <li><a href='iso/af.html'> ... </li>
+  #    <li><a href='iso/al.html'> ... </li>
+  #    ...
+  #  </ul>
+
+  # Obtenim les URLs de tots els països
+  paisos = [x.get_attribute("href") for x in navegador.find_elements_by_xpath("//*[@id='myUL']//a")]
+
+  return paisos
+
+
+# Cada taula es un objecte Selenium WebElement 
+def analitzar_taula(ISO2, taula):
+  # Només s'han d'analitzar les taules amb indicadors.
+  # S'identifiquen perque tenen la paraula "indicator" a l'element <summary>
+  summary = taula.find_element_by_tag_name("summary").text
+
+  if not re.search('indicators',summary): 
+    return;
+
+  print (summary + "...", end=" ")
+
+  # La taula presenta els indicadors en files i els anys en columnes
+  # La primera columna de la taula conté el nom de l'indicador
+  # i la resta de columnes els anys.
+
+  # Obtenim els anys de l'encapçalament de la taula
+
+  years = [x.get_attribute("innerText") for x in taula.find_elements_by_xpath("table/thead/tr/td")][1:]
+  print(years)
+
+  # Obtenim els indicadors de les files
+
+  for fila in taula.find_elements_by_xpath("table/tbody/tr"):
+
+    indicador = fila.find_element_by_tag_name("td").get_attribute("innerText").replace(u'\xa0', u' ')
+    valors = [x.get_attribute("innerText").replace(" ","") for x in fila.find_elements_by_tag_name("small")][1:]
+    print(indicador)
+    print(valors)
+
+    # Incorporar els valors al dataframe
+    
+
+
+def analitzar_pais(url_pais):
+  # les Url dels paisos tenen el format http://data.un.org/en/iso/<codiISO2>.html
+  # Obtenim el codi ISO2 (en majuscules)
+  ISO2 = re.search('(?<=iso/)..',url_pais).group(0).upper()
+
+  print("Analitzant pais " + ISO2 + " ... ", end="")
+
+  # Descarregar la pàgina del pais
+  navegador.get(url_pais)
+
+  # Els indicadors de cada pais s'organitzen en diferents taules dins d'un element <details>
+  taules = navegador.find_elements_by_tag_name("details")
+
+  for taula in taules:
+    analitzar_taula(ISO2, taula)
+
+
+
+
+# Els indicadors de cada pais s'aniran integrant en un data frame amb les columnes
+# "ISO2","year","<indicador 1","<indicador 2",...."indicador N"
+# Les columnes d'indicadors s'aniran generant dinamicament a mida que es detecti un nou indicador
+indicadors_UN = pd.DataFrame(columns=["ISO2","year"])
+
+
+paisos = obtenir_urls_paisos_UN()
+
+for pais in paisos:
+  analitzar_pais(pais)
+
+
+# Finalment tanquem la pagina del navegador
+# navegador.close()
